0ddc46b7330fa1c75badc51385f73c310fc28eef
Adam Tse <mr.adam.tse@gmail.com>
2024-08-11 21:01:44 +0800

    feat(APP): update docker

--- src/main/java/AtomJai/one/Application.java
+++ src/main/java/AtomJai/one/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OneApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OneApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/AtomJai/one/ApplicationTests.java
+++ src/test/java/AtomJai/one/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class OneApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
